b47f536426ce697dfc81f310f0eb36b227959f2c
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-19 13:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function stringChop(str, size) {
- let arr = str.split("")
+ if(str = null){
+	 return ""
+ }
+	let arr = str.split("")
 	let result = []
 	while(arr.length >0){
 		result.push(arr.splice(0,size).join(""))
@@ -8,6 +11,6 @@ return result
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
